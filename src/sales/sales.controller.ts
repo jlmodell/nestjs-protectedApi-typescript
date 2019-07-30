c364f40e2f25a73ab1959097f3a9e67447b9d7e4
jlmodell <modell.jeff@me.com>
2019-07-30 16:07:31 -0400

    added comments to routes

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -5,6 +5,7 @@ import { SalesService } from './sales.service';
 export class SalesController {
   constructor(private readonly salesService: SalesService) {}
 
+  // GET list of sales for date range
   @Get()
   async getSales(
     @Body('start') start: string,
@@ -14,6 +15,8 @@ export class SalesController {
     return await this.salesService.getSales(start, end, authHeader);
   }
 
+  // GET list of sales for specific customer id (cid) or group by date range
+  // split group by "-" eg) 1300-9988
   @Get('cust/:cid')
   async getSalesByCust(
     @Param('cid') cid: string,
@@ -24,6 +27,8 @@ export class SalesController {
     return await this.salesService.getSalesByCust(start, end, cid, authHeader);
   }
 
+  //GET list of sales for specific item id (iid) or group by date range
+  // split group by "-" eg) 298-723
   @Get('item/:iid')
   async getSalesByItem(
     @Param('iid') iid: string,
@@ -34,6 +39,7 @@ export class SalesController {
     return await this.salesService.getSalesByItem(start, end, iid, authHeader);
   }
 
+  //GET summarized detail for customer by cid
   @Get('summary/cust/:cid')
   async getSummaryByCust(
     @Param('cid') cid: string,
@@ -49,6 +55,7 @@ export class SalesController {
     );
   }
 
+  //GET summarized detail for item by iid
   @Get('summary/item/:iid')
   async getSummaryByItem(
     @Param('iid') iid: string,
@@ -64,6 +71,7 @@ export class SalesController {
     );
   }
 
+  //GET list of distinct customers in a date range
   @Get('distinct/cust')
   async getDistinctCustomers(
     @Body('start') start: string,
@@ -73,6 +81,7 @@ export class SalesController {
     return await this.salesService.getDistinctCustomers(start, end, authHeader);
   }
 
+  //GET list of distinct items in a date range
   @Get('distinct/item')
   async getDistinctItems(
     @Body('start') start: string,
