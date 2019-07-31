def0c78681f36c2ab74927ae65b2d7cfa8e7c73b
jlmodell <modell.jeff@me.com>
2019-07-31 16:37:25 -0400

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
-
-const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.enableCors();
-  await app.listen(PORT, () => {
-    Logger.log(`Http://localhost:${PORT}`, 'BootStrap');
-  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
